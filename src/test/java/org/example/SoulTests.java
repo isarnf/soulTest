8102dee8256afb4a6446b33a01a2fcd7d7ebecf3
isarnf <isarnf@gmail.com>
2023-05-25 10:52:03 -0300

    feat: initialized test project and included dependencies

---
+++ src/test/java/org/example/SoulTests.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SoulTests {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
+
+}
