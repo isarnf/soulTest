127b2ed2567d3bf655044f3d295b1999d5ff211e
Danilo Araujo <x45danilo45x@gmail.com>
2023-06-01 10:11:26 -0300

    fix: open chrome browser in newer versions

--- src/test/java/org/example/SoulTests.java
+++ src/test/java/org/example/SoulTests.java
@@ -3,8 +3,11 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SoulTests {
 
@@ -13,7 +16,9 @@ public class SoulTests {
     @BeforeEach
     void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
